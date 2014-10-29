dbf4cf38e52f383dab6d1076638bb9287885a58f
Hang Sun <sunhang36@gmail.com>
2014-10-29 16:15:15 +0800

    data set up!!!

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EmployeeEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EmployeeEntity_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-26T20:17:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-29T16:12:25")
 @StaticMetamodel(EmployeeEntity.class)
 public class EmployeeEntity_ { 
 
