6d7d1d2428c9c5e798d015e066faad9a99a00fc0
Mengdan <zhaomengdan93@gmail.com>
2014-11-05 20:56:56 +0800

    commit

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/LeaveEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/LeaveEntity_.java
@@ -6,11 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-29T13:28:28")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-29T16:12:25")
->>>>>>> dbf4cf38e52f383dab6d1076638bb9287885a58f
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-29T18:17:53")
 @StaticMetamodel(LeaveEntity.class)
 public class LeaveEntity_ { 
 
