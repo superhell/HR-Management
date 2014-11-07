896f8b4276657494d7a70ec08ce6c3a384390c86
Mengdan <zhaomengdan93@gmail.com>
2014-11-07 19:07:09 +0800

    hehe

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EventEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EventEntity_.java
@@ -5,15 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-06T17:22:45")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-06T18:49:14")
->>>>>>> 7c722535f91f2ef313deed5b9df93efb8d100522
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-06T18:49:14")
->>>>>>> 7c722535f91f2ef313deed5b9df93efb8d100522
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-06T21:52:23")
 @StaticMetamodel(EventEntity.class)
 public class EventEntity_ { 
 
