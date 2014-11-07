58d28c1863bf8a298738109fcb9114714f1fda19
Hang Sun <sunhang36@gmail.com>
2014-11-07 22:39:17 +0800

    =.=

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/CheckInEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/CheckInEntity_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-07T19:04:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-07T19:59:33")
 @StaticMetamodel(CheckInEntity.class)
 public class CheckInEntity_ { 
 
