f44cec0ee54b566ff2b73e22a7cc9d2072e22ad9
Zhang Shiyu <ms.z.summer@gmail.com>
2014-11-07 12:31:03 +0800

    change in employeebean

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/SalaryEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/SalaryEntity_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-06T01:34:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-06T17:37:33")
 @StaticMetamodel(SalaryEntity.class)
 public class SalaryEntity_ { 
 
