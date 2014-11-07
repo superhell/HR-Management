e683efe6691e176e2537a58a0a5c3a3b853c7bb4
Mengdan <zhaomengdan93@gmail.com>
2014-11-08 06:09:01 +0800

    commit

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/CheckInEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/CheckInEntity_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-07T20:31:21")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-07T23:23:05")
 @StaticMetamodel(CheckInEntity.class)
 public class CheckInEntity_ { 
 
