f8d6c7eb3bf072ffa3f3598f8f0737cc76eba65a
Zhang Shiyu <ms.z.summer@gmail.com>
2014-11-07 12:47:44 +0800

    merge conflict

--- HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EventEntity_.java
+++ HRApp/HRApp-ejb/build/generated-sources/ap-source-output/Entity/EventEntity_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-06T17:37:33")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-06T23:16:54")
->>>>>>> 9448e08097328a0550f44bb4d4b0c34204a70d7b
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-07T12:32:51")
 @StaticMetamodel(EventEntity.class)
 public class EventEntity_ { 
 
